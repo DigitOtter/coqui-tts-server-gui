40b621ac79492a0be8bebaa516c03bb7b13d4913
Digit Otter <digitotter@protonmail.com>
2022-10-30 05:36:59 +0100

    Add windows install script

---
+++ install/build_win.bat
@@ -0,0 +1,1 @@
+pyinstaller --collect-all TTS --collect-data trainer --add-data '../src/coqui_tts_server_gui/config.ini;coqui_tts_server_gui' --add-data '../src/coqui_tts_server_gui/icon.ico;.' --add-binary 'tts-server.exe;coqui_tts_server_gui\tts-server-bin' --noconfirm -F ../src/coqui_tts_server_gui/app.py
