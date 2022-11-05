abc41abe7ce1b48febfe6d8f857f9d7de72fca23
Digit Otter <digitotter@protonmail.com>
2022-11-05 21:48:10 +0100

    Only allow one instance to start

--- src/coqui_tts_server_gui/app.py
+++ src/coqui_tts_server_gui/app.py
@@ -1,18 +1,52 @@
 from PyQt6 import QtWidgets
 from PyQt6.QtWidgets import QSystemTrayIcon, QMenu
 from PyQt6.QtGui import QIcon, QAction
+from PyQt6.QtCore import QSystemSemaphore, QSharedMemory
 
 import sys
 import os
 
-from matplotlib.pyplot import show
-
 from coqui_tts_server_gui.gui.main_window import MainWindow
 
+
+def check_if_running(app_name: str, shared_memory: QSharedMemory) -> bool:
+    """
+    Ensures that only one instance of app is started.
+    Code taken from https://stackoverflow.com/questions/65717815/how-can-i-make-my-pyqt5-app-oney-one-instance
+    """
+    semaphore = QSystemSemaphore(app_name, 1)
+    semaphore.acquire()  # Raise the semaphore, barring other instances to work with shared memory
+
+    if sys.platform != 'win32':
+        # in linux / unix shared memory is not freed when the application terminates abnormally,
+        # so you need to get rid of the garbage
+        nix_fix_shared_mem = QSharedMemory(shared_memory.key())
+        if nix_fix_shared_mem.attach():
+            nix_fix_shared_mem.detach()
+
+    if shared_memory.attach(QSharedMemory.AccessMode.ReadWrite):  # attach a copy of the shared memory, if successful, the application is already running
+        is_running = True
+    else:
+        shared_memory.create(1, QSharedMemory.AccessMode.ReadWrite)  # allocate a shared memory block of 1 byte
+        is_running = False
+
+    semaphore.release()
+
+    return is_running
+
+
 if __name__ == '__main__':
+    app_name: str = 'coqui_ai_tts_server_gui'
+
     app = QtWidgets.QApplication(sys.argv)
+
+    shared_memory = QSharedMemory("coqui_ai_tts_server_gui_shmem")
+    if check_if_running(app_name, shared_memory):
+        print("CoquiAI TTS GUI already running")
+        sys.exit(0)
+
     app.setQuitOnLastWindowClosed(False)
-    app.setApplicationName('coqui_ai_tts_server_gui')
+    app.setApplicationName(app_name)
     app.setOrganizationName('digitotter')
 
     cwd = os.path.dirname(os.path.abspath(__file__))
