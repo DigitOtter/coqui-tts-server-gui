98bc793ae22fe41cfe88a5882a64d94268788e8a
Digit Otter <digitotter@protonmail.com>
2022-11-05 16:41:31 +0100

    Update submodule to fix Windows multiprocessing

--- install/build_win.bat
+++ install/build_win.bat
@@ -1,2 +1,1 @@
-pyinstaller --collect-all TTS --collect-data trainer --add-data "..\src\coqui_tts_server_gui\config.ini;coqui_tts_server_gui" --add-data "..\src\coqui_tts_server_gui\icon.ico;." --add-binary "tts-server.exe;coqui_tts_server_gui\tts-server-bin" --hidden-import coqui_tts_server_gui --hidden-import PyQt6 --collect-all PyQt6 --noconfirm -F ..\src\coqui_tts_server_gui\app.py
-
+pyinstaller --collect-all TTS --collect-data trainer --add-data "..\src\coqui_tts_server_gui\config.ini;coqui_tts_server_gui" --add-data "..\src\coqui_tts_server_gui\icon.ico;." --add-binary "tts-server.exe;coqui_tts_server_gui\tts-server-bin" --hidden-import coqui_tts_server_gui --hidden-import PyQt6 --noconfirm --windowed --python-option "W ignore" -F ..\src\coqui_tts_server_gui\app.py
